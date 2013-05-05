a566997b13c43303e85135dc9e46c18371ce5606
Nikolaus West <nikolaus.west@me.com>
2013-05-05 16:42:05 -0700

    Added gaussian weighted l2 norm calculation to main branch

---
+++ code/trunk/deNoise2D_NLM_GW.m
@@ -0,0 +1,117 @@
+function output = deNoise2D_NLM_GW( noisyImg, config, origImg )
+%Uses gaussian weighted L2 norm
+%Does not work with color for now
+
+assert(config.color == false);
+
+  kSize = config.kSize;
+  searchSize = config.searchSize;
+  h = config.h;
+  noiseSig = config.noiseSig;
+  color = config.color;
+
+  halfSearchSize = floor( searchSize/2 );
+  halfKSize = floor( kSize/2 );
+  hSq = h*h;
+
+ 
+  [M N] = size( noisyImg );
+  
+  %Define the gaussian kernel for the gaussian weighted L2-norm
+  a = 0.5*(kSize-1)/2;
+  gaussKernel = fspecial('gaussian', kSize, a);
+
+  deNoisedImg = noisyImg;
+
+  borderSize = halfKSize+halfSearchSize+1;
+  
+  %% initialize progress tracker
+  try % Initialization
+    ppm = ParforProgressStarter2(config.fileName, M-2*borderSize, 0.1);
+  catch me % make sure "ParforProgressStarter2" didn't get moved to a different directory
+    if strcmp(me.message, 'Undefined function or method ''ParforProgressStarter2'' for input arguments of type ''char''.')
+      error('ParforProgressStarter2 not in path.');
+    else
+      % this should NEVER EVER happen.
+      msg{1} = 'Unknown error while initializing "ParforProgressStarter2":';
+      msg{2} = me.message;
+      print_error_red(msg);
+      % backup solution so that we can still continue.
+      ppm.increment = nan(1, N);
+    end
+  end
+
+
+  %% perform algorithm
+  parfor j=borderSize:M-borderSize
+    for i=borderSize:N-borderSize
+      % As far as I (Thomas) know, noisyImg can't be easily sliced to
+      % improve performance. Instead, one would have to use spmd to do
+      % such things. However, most of the time is spent in the two inner 
+      % loops anyway 
+      
+      
+      
+      kernel = noisyImg( j-halfKSize:j+halfKSize, ...
+          i-halfKSize:i+halfKSize );
+      localWeights = zeros( searchSize, searchSize );
+      
+      
+
+      for jP=0:searchSize-1
+        for iP=0:searchSize-1
+          %disp(['(jP,iP): (',num2str(jP),',',num2str(iP),')']);
+
+          vJ = j-halfSearchSize+jP;
+          vI = i-halfSearchSize+iP;
+          
+          
+          v = noisyImg( vJ-halfKSize : vJ+halfKSize, ...
+              vI-halfKSize : vI+halfKSize  );
+          
+          %Gaussian weighted L2 norm squared
+          distSq = ( kernel - v ) .* ( kernel - v );
+          weightedDistSq = distSq.*gaussKernel;
+          weightedDistSq = sum( weightedDistSq(:) ); 
+                
+          localWeights( jP+1, iP+1 ) = exp( - weightedDistSq / hSq );
+
+        end
+      end
+
+      localWeights = localWeights / sum( localWeights(:) );
+      
+      subImg = noisyImg( j-halfSearchSize : j+halfSearchSize, ...
+          i-halfSearchSize : i+halfSearchSize, : );
+      
+      deNoisedImg(j,i,:) = sum( sum( localWeights .* subImg ) ) ;
+      
+    end
+
+    ppm.increment(j);
+  end
+  
+  
+  %% clean up parallel
+  try % use try / catch here, since delete(struct) will raise an error.
+      delete(ppm);
+  catch me %#ok<NASGU>
+  end
+  
+
+  %% show output image
+  imshow( deNoisedImg, [] );
+  drawnow; % make sure it's displayed
+  pause(0.01); % make sure it's displayed
+  
+  %% copy output images
+  output = struct();
+  output.deNoisedImg = deNoisedImg;
+  output.prefix = 'NLM_';
+
+  output.mse = -1;
+  if nargin > 2
+    output.mse = calculateMSE( origImg, deNoisedImg, borderSize );
+  else
+  end
+end
