dcbbeccbba6f74ad4807885914c41f3509ea0b8e
Thomas Walther <earthnail@gmail.com>
2013-05-14 16:50:21 -0700

    The NLM algorithm works now. However, it converts the image to floating point numbers. For memory efficiency, we want to calculate with unsigned chars. Still, it works.

--- code/cplusplus/nlmxcode/nlmxcode/StandardNLMAlgorithm.cpp
+++ code/cplusplus/nlmxcode/nlmxcode/StandardNLMAlgorithm.cpp
@@ -18,12 +18,25 @@
 #include "TestConfig.h"
 
 
+uchar roundToChar(float d) {
+    int i = round(d);
+    i = i > 255 ? 255 : i;
+    i = i < 0 ? 0 : i;
+    return (uchar)i;
+}
+
+
 StandardNLMAlgorithm::StandardNLMAlgorithm(TestConfig config)
 : NLMAlgorithm(config) {
 }
 
 Mat StandardNLMAlgorithm::runAlgorithm(Mat noisyImg) {
     printf("StandardNLMAlgorithm::runAlgorithm\n");
+    
+    // because OpenCV elementwise matrix multiplication only works when both matrices
+    // have the same type, we need to convert our noisyImg to the same  type of the local
+    // weights.
+    noisyImg.convertTo(noisyImg, CV_32F);
     
     int kSize = config.kSize;
     int searchSize = config.searchSize;
@@ -33,7 +46,7 @@ Mat StandardNLMAlgorithm::runAlgorithm(Mat noisyImg) {
     
     int halfSearchSize = searchSize/2; // integer division floors automatically
     int halfKSize = kSize/2;; // integer division floors automatically
-    double hSq = h * h;
+    float hSq = h * h;
     
     // TODO: removed color code because I don't know how openCV handles three
     // dimensional matrices.
@@ -52,6 +65,11 @@ Mat StandardNLMAlgorithm::runAlgorithm(Mat noisyImg) {
     Mat kernel;
     Mat localWeights;
     
+    
+    // local weights are floating point numbers
+    // TODO: see if we can pack them in a single byte of memory.
+    localWeights = Mat::zeros(searchSize, searchSize, CV_32F);
+    
     // perform algorithm
     // both i, j loops can easily be parallelized
     for(int j = borderSize - 1; j < M - borderSize; j++) {
@@ -59,9 +77,6 @@ Mat StandardNLMAlgorithm::runAlgorithm(Mat noisyImg) {
         for(int i = borderSize - 1; i < N - borderSize; i++) {
             kernel = noisyImg.rowRange(j-halfKSize, j+halfKSize+1)
                              .colRange(i-halfKSize, i+halfKSize+1);
-            // local weights are floating point numbers
-            // TODO: see if we can pack them in a single byte of memory.
-            localWeights = Mat::zeros(searchSize, searchSize, CV_32F);
             
             for(int jP = 0; jP < searchSize; jP++) {
                 for(int iP = 0; iP < searchSize; iP++) {
@@ -71,35 +86,30 @@ Mat StandardNLMAlgorithm::runAlgorithm(Mat noisyImg) {
                     Mat v = noisyImg.rowRange(vJ - halfKSize, vJ + halfKSize + 1)
                                     .colRange(vI - halfKSize, vI + halfKSize + 1);
                     
-                    uchar distSq = sum((kernel - v).mul(kernel - v))[0];
+                    float distSq = sum((kernel - v).mul(kernel - v))[0];
                     
                     // TODO: see if we can pack the weights in a single byte of memory.
                     // Idea: The exponential is always < 1. We can multiply the result by 255.
-                    localWeights.at<float>(jP, iP) = exp(-((double)distSq) / hSq);
+                    localWeights.at<float>(jP, iP) = exp(-((float)distSq) / hSq);
                 }
             }
             
-            localWeights = localWeights.mul(Mat::ones(localWeights.size(), CV_32F), 1.0/sum(localWeights)[0]);
+            double scale = 1.0/sum(localWeights)[0];
             
             Mat subImg = noisyImg.rowRange(j-halfSearchSize, j + halfSearchSize + 1)
             .colRange(i-halfSearchSize, i + halfSearchSize + 1);
             
-            // because OpenCV elementwise matrix multiplication only works when both matrices
-            // have the same type, we need to convert our subImg to the same  type of the local
-            // weights.
-            subImg.convertTo(subImg, CV_32F);
-            
             // TODO: make this work with a localWeights matrix of type uchar
             // Idea: Do the elementwise summing manually. That way, we can cast in every iteration to a
-            // short/int because sure the element multiplication would exceed the uchar bounds.
+            // short/int because otherwise the element multiplication would exceed the uchar bounds.
             // Then, at the very end, divide by 255 (since we multiplied all weights by 255)
-            int temp = round(sum(localWeights.mul(subImg))[0]);
-            temp = temp > 255 ? 255 : temp;
-            temp = temp < 0 ? 0 : temp;
-            
-            deNoisedImg.at<uchar>(j,i) = temp;
+            deNoisedImg.at<float>(j,i) = sum(localWeights.mul(subImg, scale))[0];
         }
-    }    
+    }
+    
+    // convert back to uchar;
+    deNoisedImg.convertTo(deNoisedImg, CV_8U);
+    
     return deNoisedImg;
 }
 
