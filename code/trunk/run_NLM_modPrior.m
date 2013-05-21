f2b383918f09703e7c864f6dbecec07bef023e4a
Nicholas Dwork <ndwork@hotmail.com>
2013-05-20 22:17:33 -0700

    Implemented NLM with a modified prior based on correlation and variance.

---
+++ code/trunk/run_NLM_modPrior.m
@@ -0,0 +1,26 @@
+function run_NLM_modPrior
+
+  % FUNCTION HANDLE
+  algorithmHandle = @deNoise2D_NLM_modPrior;
+
+  % NLM CONFIGURATION VALUES (NOMINAL)
+  config = struct();
+  config.kSize = 7;
+  config.searchSize = 21; %nominal value is 21
+  config.noiseSig = 20/255; %standard deviation!
+  config.h = 12*config.noiseSig;
+  config.noiseMean = 0;
+
+  % TEST SUITE CONFIGURATION
+  config.testSuiteAddNoise = true; %if false, will not add noise to the image. used when imputting images with noise already present.
+  config.testSuiteUseExternalImage = false; %if true, will not read in any images, but will process based on what you pass in
+  config.color = false; %if true, will not convert to gray scale and will compute similarities based on color (RGB)
+
+  %test 1
+  config.noiseSig = 20/255;
+  config.h = 12*config.noiseSig;
+  config.testSuiteUseImages = {'barbara.png','lena.png','mandrill.png', ...
+    'comedian.png', 'boat.png', 'airplane.bmp', 'goldhill.bmp'};
+  test_suite(algorithmHandle, config);
+
+end
