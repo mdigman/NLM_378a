c5718e58ea942a6b353cf7e17f3aaf197d676bed
Nicholas Dwork <ndwork@hotmail.com>
2013-05-21 23:16:52 -0700

    Adding functionality to denoise MRI data.

---
+++ code/trunk/deNoiseMRI_NLM.m
@@ -0,0 +1,76 @@
+function output = deNoiseMRI_NLM( noisyMRI, config )
+
+  kSize = config.kSize;
+  searchSize = config.searchSize;
+  h = config.h;
+  noiseSig = config.noiseSig;
+
+  halfSearchSize = floor( searchSize/2 );
+  halfKSize = floor( kSize/2 );
+  hSq = h*h;
+
+  [K M N] = size( noisyMRI );
+
+  deNoisedMRI = noisyMRI;
+
+  borderSize = halfKSize+halfSearchSize+1;
+
+
+  %-- perform algorithm
+  for k=borderSize:K-borderSize
+    disp(['Working on slice, ', num2str(k)]);
+    
+    for j=borderSize:M-borderSize
+      disp(['Working on slice/row ', num2str(k), ', ', num2str(j)]);
+
+      for i=borderSize:N-borderSize
+        %disp(['Working on slice/row/col ', ...
+        %  num2str(k), ', ', num2str(j), ', ', num2str(i)]);
+
+        kernel = noisyMRI( k-halfKSize:k+halfKSize, ...
+          j-halfKSize:j+halfKSize, ...
+          i-halfKSize:i+halfKSize, :);
+        localWeights = zeros( searchSize, searchSize, searchSize );
+
+        for kP=0:searchSize-1
+          for jP=0:searchSize-1
+            for iP=0:searchSize-1
+              %disp(['(jP,iP): (',num2str(jP),',',num2str(iP),')']);
+
+              vK = k-halfSearchSize+kP;
+              vJ = j-halfSearchSize+jP;
+              vI = i-halfSearchSize+iP;
+
+              v = noisyMRI( vK-halfKSize : vK+halfKSize, ...
+                vJ-halfKSize : vJ+halfKSize, ...
+                vI-halfKSize : vI+halfKSize );
+
+              tmp = kernel - v;
+              distSq = tmp .* tmp;
+              distSq = sum( distSq(:) ); %L2 norm squared
+
+              localWeights( kP+1, jP+1, iP+1 ) = distSq;
+            end
+          end
+        end
+
+        localWeights = exp( -localWeights / hSq );
+        localWeights = localWeights / sum( localWeights(:) );
+
+        subMRI = noisyMRI( k-halfSearchSize : k+halfSearchSize, ...
+            j-halfSearchSize : j+halfSearchSize, ...
+            i-halfSearchSize : i+halfSearchSize, : );
+
+        deNoisedMRI(k,j,i) = sum( localWeights(:) .* subMRI(:) );
+      end
+
+    end
+  end
+
+  %-- copy output images
+  output = struct();
+  output.deNoisedMRI = deNoisedMRI;
+  output.prefix = 'NLM_';
+  output.borderSize = borderSize;
+
+end
