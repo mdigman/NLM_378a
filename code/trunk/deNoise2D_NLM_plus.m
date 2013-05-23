2f5fea1d9cf0d9c250112a03bbe6da4337b9ffbf
Nikolaus West <nikolaus.west@me.com>
2013-05-23 16:07:46 -0700

    Speedup for BNLM

--- code/trunk/deNoise2D_NLM_plus.m
+++ code/trunk/deNoise2D_NLM_plus.m
@@ -1,19 +1,16 @@
-function output = deNoise2D_BNLM( noisyImg, config, origImg )
+function output = deNoise2D_NLM_plus( noisyImg, config )
 
   kSize = config.kSize;
   searchSize = config.searchSize;
+  h = config.h;
+  noiseSig = config.noiseSig;
   color = config.color;
 
   halfSearchSize = floor( searchSize/2 );
   halfKSize = floor( kSize/2 );
   
-  %Parameters for BNLM 
-  noiseSig = config.noiseSig;
   bayes_dist_offset = sqrt(2*kSize^2 -1);
-  mean_thresh = 3*noiseSig/kSize;
-  F_thresh = 1.6;
-  kSq = kSize^2;
-
+  
   if color
     [M N C] = size( noisyImg );
   else
@@ -52,75 +49,44 @@ function output = deNoise2D_BNLM( noisyImg, config, origImg )
       if color
         kernel = noisyImg( j-halfKSize:j+halfKSize, ...
           i-halfKSize:i+halfKSize, :);
-        localWeights = zeros( searchSize, searchSize , 3);
-        
-        %TODO: Check if this is correct for color
-        kernel_mean = mean(kernel(:));
-        kernel_var = var(kernel(:));
+        %localWeights = zeros( searchSize, searchSize , 3);
+        dists = zeros( searchSize, searchSize , 3);
       else
         kernel = noisyImg( j-halfKSize:j+halfKSize, ...
           i-halfKSize:i+halfKSize );
-        localWeights = zeros( searchSize, searchSize );
-        
-        kernel_mean = mean(kernel(:));
-        kernel_var = var(kernel(:));
+        %localWeights = zeros( searchSize, searchSize );
+        dists = zeros( searchSize, searchSize);
       end
       
-      %Approximate central weight (the weight where the Kernel is compared
-      %to itself) with the best non central weight.
-      max_weight = 0;
+      
 
-      %TODO: Do mean and variance (F) test 
       for jP=0:searchSize-1
         for iP=0:searchSize-1
-          if (jP ~= halfSearchSize && iP ~= halfSearchSize)
-            %disp(['(jP,iP): (',num2str(jP),',',num2str(iP),')']);
+          %disp(['(jP,iP): (',num2str(jP),',',num2str(iP),')']);
 
-            vJ = j-halfSearchSize+jP;
-            vI = i-halfSearchSize+iP;
-
-            if color
-              v = noisyImg( vJ-halfKSize : vJ+halfKSize, ...
-                vI-halfKSize : vI+halfKSize, : );
-            else
-              v = noisyImg( vJ-halfKSize : vJ+halfKSize, ...
-                vI-halfKSize : vI+halfKSize  );
-            end
-            
-            %Discard patches with too different mean or variance from
-            %dictionary
-            patch_mean = mean(v(:));
-            
-            if(abs(patch_mean - kernel_mean) > mean_thresh)
-              localWeights( jP+1, iP+1 ,:) = 0;
-            else
-              patch_var = sum((v(:)-patch_mean).^2)/kSq;
-              F = max(patch_var,kernel_var)/min(patch_var,kernel_var);
-              if (F > F_thresh)
-                localWeights( jP+1, iP+1 ,:) = 0;
-              else
-                distSq = ( kernel - v ) .* ( kernel - v );
-                dist = sqrt(sum( distSq(:) )); %L2 distance
-
-                %Non-vectorized Bayesian Non-Local means weights
-                weight = exp( -0.5*(dist/noiseSig - bayes_dist_offset)^2 );
-                localWeights( jP+1, iP+1 ,:) = weight;
-
-                %Update max-weight
-                max_weight = max(max_weight, weight);
-              end
-            end
+          vJ = j-halfSearchSize+jP;
+          vI = i-halfSearchSize+iP;
+          
+          if color
+            v = noisyImg( vJ-halfKSize : vJ+halfKSize, ...
+              vI-halfKSize : vI+halfKSize, : );
+          else
+            v = noisyImg( vJ-halfKSize : vJ+halfKSize, ...
+              vI-halfKSize : vI+halfKSize  );
           end
+          distSq = ( kernel - v ) .* ( kernel - v );
+          dists( jP+1, iP+1 ,:) = sqrt(sum( distSq(:) )); %L2 distance
+
+          
+          %weight = exp( -0.5*(dist/noiseSig - bayes_dist_offset)^2 );
+          %localWeights( jP+1, iP+1 ,:) = weight;
+
         end
       end
       
-      %Set central weight
-      if max_weight == 0
-        localWeights(halfSearchSize + 1, halfSearchSize +1,:) = 1;
-      else
-        localWeights(halfSearchSize + 1, halfSearchSize +1,:) = max_weight;
-      end
-        
+      %Non-vectorized Bayesian Non-Local means weights
+      localWeights = exp( -0.5*(dists/noiseSig - bayes_dist_offset).^2 );
+      
       if color
         localWeights = localWeights / sum( sum( localWeights(:,:,1) ) ); 
       else
