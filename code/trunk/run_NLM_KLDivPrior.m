7e485e67d91d8bd32900a78780f4f5b8d787a51d
Nikolaus West <nikolaus.west@me.com>
2013-05-28 12:20:43 -0700

    Added code to test a prior based on the KL-Divergence

---
+++ code/trunk/run_NLM_KLDivPrior.m
@@ -0,0 +1,53 @@
+function run_NLM_KLDivPrior
+
+% FUNCTION HANDLE
+algorithmHandle = @deNoise2D_NLM_KLDivPrior;
+
+% NLM CONFIGURATION VALUES (NOMINAL)
+config = struct();
+config.kSize = 7;
+config.searchSize = 21; %nominal value is 21
+config.noiseSig = 20/255; %standard deviation!
+config.h = 12*config.noiseSig;
+config.noiseMean = 0;
+
+
+% TEST SUITE CONFIGURATION
+config.testSuiteAddNoise = true; %if false, will not add noise to the image. used when imputting images with noise already present.
+config.testSuiteUseExternalImage = false; %if true, will not read in any images, but will process based on what you pass in
+config.color = false; %if true, will not convert to gray scale and will compute similarities based on color (RGB)
+
+for KLexp = 1:3
+  for KLh = 1:0.5:4
+    
+    config.KLh = KLh;
+    config.KLexp = KLexp;
+
+    %test 1
+    config.noiseSig = 8/255;
+    config.h = 12*config.noiseSig;
+    config.testSuiteUseImages = {'boat.png'};
+    test_suite(algorithmHandle, config);
+
+    %test 2
+    config.noiseSig = 20/255;
+    config.h = 12*config.noiseSig;
+    config.testSuiteUseImages = {'lena.png'};
+    test_suite(algorithmHandle, config);
+
+    %test 3
+    config.noiseSig = 25/255;
+    config.h = 12*config.noiseSig;
+    config.testSuiteUseImages = {'barbara.png'};
+    test_suite(algorithmHandle, config);
+
+    %test 4
+    config.noiseSig = 35/255;
+    config.h = 12*config.noiseSig;
+    config.testSuiteUseImages = {'mandrill.png'};
+    test_suite(algorithmHandle, config);
+
+  end
+end
+
+end
